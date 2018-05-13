8df8b1c27e69aa3bb4e20b8a26aefd9ed33ae857
Erick Almeida <ephillipe@gmail.com>
2018-05-13 15:22:38 -0300

    Falta  oteste

--- package.cmd
+++ package.cmd
@@ -1,5 +1,5 @@
 
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" .\eventflit-dotnet-everything-server.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" .\eventflit-dotnet-everything-server.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
 
 tools\nuget.exe update -self
 tools\nuget.exe pack .\EventflitServer\EventflitServer.csproj -verbosity detailed -properties Configuration=Release
